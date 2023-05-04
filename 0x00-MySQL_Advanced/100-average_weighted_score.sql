e0fc8b250f93fedad16185eb14cbca6849dab4d2
kennywindis <kennyphilip100@gmail.com>
2023-05-04 16:58:56 -0700

    sql

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,17 +1,15 @@
--- SQL script that creates stored procedure ComputeAverageWeightedScoreForUsers
--- that computes and store the average weighted score for all students
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
+-- SQL script that creates a stored procedure ComputeAverageWeightedScoreForUse that computes and store the average weighted score for a student.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER |
-CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+  user_id INT)
 BEGIN
-  UPDATE users AS U, 
-    (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
+  DECLARE w_avg_score FLOAT;
+  SET w_avg_score = (SELECT SUM(score * weight) / SUM(weight)
     FROM users AS U 
     JOIN corrections as C ON U.id=C.user_id 
     JOIN projects AS P ON C.project_id=P.id 
-    GROUP BY U.id)
-  AS WA
-  SET U.average_score = WA.w_avg 
-  WHERE U.id=WA.id;
+    WHERE U.id=user_id);
+  UPDATE users SET average_score = w_avg_score WHERE id=user_id;
 END;
 |
