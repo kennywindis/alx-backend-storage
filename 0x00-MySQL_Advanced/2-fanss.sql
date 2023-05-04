84f63ccf85329df177fae4266bf32f2391ae2b81
kennywindis <kennyphilip100@gmail.com>
2023-05-04 08:25:56 -0700

    sql

---
+++ 0x00-MySQL_Advanced/2-fanss.sql
@@ -0,0 +1,6 @@
+-- sql that ranks country origins of bands
+-- ordered by the number of (non-unique) fans
+
+SELECT DISTINCT 'origin', SUM('fans') as 'nb-fans' FROM 'metal_bands'
+GROUP BY 'origin'
+ORDER BY 'nb_fans' DESC;
