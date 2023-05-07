9433c3ddbc075adf97ccd24ac253ca74a724f9f7
kennywindis <kennyphilip100@gmail.com>
2023-05-07 05:51:57 -0700

    sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,5 @@
--- sql script that lists all bands with Glam rock as their main style,ranked by their longevity
+--SQL script that lists all bands using Glam rock as their main style, ranked bytheir longevity
+
 SELECT DISTINCT `band_name`,
 IFNULL(`split`, 2020) - `formed` as `lifespan`
   FROM `metal_bands` WHERE FIND_IN_SET('Glam rock', style)
