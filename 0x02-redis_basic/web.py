2c0870909bebc5e37282d46a5a8f189de7b374f2
kennywindis <kennyphilip100@gmail.com>
2023-05-10 20:18:50 -0700

    rb

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,10 +13,11 @@ def get_page(url: str) -> str:
     """ track how many times a particular URL was accessed in the key
         "count:{url}"
         and cache the result with an expiration time of 10 seconds """
-    r.set(f"cached:{url}", count)
+    r.set(f"count:{url}", 0)
     resp = requests.get(url)
     r.incr(f"count:{url}")
-    r.setex(f"cached:{url}", 10, r.get(f"cached:{url}"))
+    r.setex(f"cached:{url}", 10, 
+    r.get(f"cached:{url}"))
     return resp.text
 
 
